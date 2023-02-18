110e935014e0754e15e732db48bc4bd2e32ff1bb
Haley <habel6@ivytech.edu>
2023-02-17 23:41:09 -0500

    Added changes

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\haley\Classes Spring 2023\Python\Module 04\djangogirls\myvenv
+set VIRTUAL_ENV=C:\Users\haley\Classes Spring 2023\Python\Module 05\djangogirls\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
